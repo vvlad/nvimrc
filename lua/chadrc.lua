f1474e67e826676212ddda7df08596b7fe981847
Vlad Verestiuc <vlad@verestiuc.com>
2024-05-11 23:59:01 +0300

    refactors nvchadrc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -3,42 +3,7 @@
 
 ---@type ChadrcConfig
 local M = {}
-
-local api = vim.api
-
-local function nvim_tree_width()
-  for _, win in pairs(api.nvim_tabpage_list_wins(0)) do
-    if vim.bo[api.nvim_win_get_buf(win)].ft == "NvimTree" then
-      return api.nvim_win_get_width(win) + 1
-    end
-  end
-  return 0
-end
-
-local function txt(str, hl)
-  str = str or ""
-  local a = "%#" .. hl .. "#" .. str
-  return a
-end
-
-local function btn(str, hl, func, arg)
-  str = hl and txt(str, hl) or str
-  arg = arg or ""
-  return "%" .. arg .. "@" .. func .. "@" .. str .. "%X"
-end
-
-vim.cmd [[
-function! NeoTestToggle(a,b,c,d)
-  lua require("neotest").summary.toggle()
-endfunction
-]]
-
-vim.api.nvim_create_user_command("NeoTestToggle", function()
-  pcall(require("neotest").summary.toggle)
-end, { nargs = 0 })
-vim.api.nvim_create_user_command("NeoTestRunAll", function()
-  require("neotest").run.run { suite = true }
-end, { nargs = 0 })
+local helper = require "helpers.nvchad"
 
 M.ui = {
   theme = "vscode_dark",
@@ -56,16 +21,8 @@ M.ui = {
     lazyload = false,
     order = { "title", "buffers", "tabs", "tests", "btns" },
     modules = {
-      title = function()
-        local title = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-        local len = nvim_tree_width()
-        title = "  " .. title .. string.rep(" ", nvim_tree_width())
-        return "%#NvimTreeRootFolder#" .. string.sub(title, 1, len)
-      end,
-      tests = function()
-        local runall = btn(" 󱇯 ", "DiagnosticHint", "NeoTestToggle")
-        return runall
-      end,
+      title = helper.title,
+      tests = helper.tests,
     },
   },
 
@@ -78,7 +35,7 @@ M.ui = {
   },
 
   nvdash = {
-    load_on_startup = false, -- vim.g.project_directory or false,
+    load_on_startup = false,
     header = { "                                            " },
 
     buttons = {
---
+++ lua/helpers/nvchad.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+local function nvim_tree_width()
+  local api = vim.api
+  for _, win in pairs(api.nvim_tabpage_list_wins(0)) do
+    if vim.bo[api.nvim_win_get_buf(win)].ft == "NvimTree" then
+      return api.nvim_win_get_width(win) + 1
+    end
+  end
+  return 0
+end
+
+vim.cmd [[
+function! NeoTestToggle()
+  lua require("neotest").summary.toggle()
+endfunction
+]]
+
+M.title = function()
+  local title = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  local len = nvim_tree_width()
+  title = "  " .. title .. string.rep(" ", nvim_tree_width())
+  return "%#NvimTreeRootFolder#" .. string.sub(title, 1, len)
+end
+
+M.txt = function(str, hl)
+  str = str or ""
+  local a = "%#" .. hl .. "#" .. str
+  return a
+end
+
+M.btn = function(str, hl, func, arg)
+  str = hl and M.txt(str, hl) or str
+  arg = arg or ""
+  return "%" .. arg .. "@" .. func .. "@" .. str .. "%X"
+end
+
+M.tests = function()
+  return M.btn(" 󱇯 ", "DiagnosticHint", "NeoTestToggle")
+end
+
+return M
